55d3ff091028318a0dc907e375bd8a94355fed38
alexn123 <alexn@tlu.ee>
2023-03-27 01:21:51 +0300

    auth single user

--- src/env.mjs
+++ src/env.mjs
@@ -21,7 +21,7 @@ const server = z.object({
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
-  USER_ID: z.string(),
+  //USER_ID: z.string(),
 });
 
 /**
@@ -45,7 +45,7 @@ const processEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  USER_ID: process.env.USER_ID,
+  //USER_ID: process.env.USER_ID,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
